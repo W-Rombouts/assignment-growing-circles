9795f400680192627f4835c50df2e8925e2f5903
Wouter Rombouts <wouter_rombouts@hotmail.com>
2020-03-26 10:15:16 +0100

    Finished Assignment

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 let myData = [1,2,3,4,5];
+
+
+var svgContainer = d3.select("body").append("svg")
+  .attr("width", 200)
+  .attr("height", 200);
+
+
+for (let i = 0; i < myData.length; i++) {
+  pos = 25*myData[i]
+  color = ""
+  if (i%2 !=0) {
+    color = "red"
+  }
+  else{
+    color = "green"
+  }
+  svgContainer.append("circle")
+  .attr("cx", pos)
+  .attr("cy", pos)
+  .attr("r", 25)
+  .style("fill", color);
+}
+
